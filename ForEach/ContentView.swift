4fe97ce06ea3219e4879bb0c78e2e439068a868e
Didier <did35@yahoo.com>
2022-03-03 21:08:11 +0100

    Initial Commit

---
+++ ForEach/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForEach
+//
+//  Created by Didier Delhaisse on 03/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ForEach/ForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachApp.swift
+//  ForEach
+//
+//  Created by Didier Delhaisse on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
